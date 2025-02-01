c88efec056fa35cdafa559027f8e7d7cd718e284
MdRahi99 <mdrahi181199@gmail.com>
2025-02-02 00:33:31 +0600

    modify

---
+++ app/Home/Process/Process.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Process = () => {
+    return (
+        <div className='max-w-7xl mx-auto py-12'>
+            Coming Soon...
+        </div>
+    );
+};
+
+export default Process;
--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -4,12 +4,14 @@ import Contact from './Contact/Contact';
 import SubBanner from './Banner/SubBanner';
 import Choose from './Choose/Choose';
 import Companies from './Companies/Companies';
+import Process from './Process/Process';
 
 const Home = () => {
     return (
-        <div className=''>
+        <div>
             <Banner />
             <SubBanner />
+            <Process />
             <Choose />
             <Companies />
             <Contact />
