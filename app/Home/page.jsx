993266bf49ddb2ab0107c22c5fa32caa8b2ea0df
MdRahi99 <mdrahi181199@gmail.com>
2025-02-01 20:32:58 +0600

    create sub banner

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Contact from './Contact/Contact';
+import SubBanner from './Banner/SubBanner';
 
 const Home = () => {
     return (
         <div className=''>
             <Banner />
+            <SubBanner />
             <Contact />
         </div>
     );
